0ba9fb9f73218ef84b0a4807792a5e6d7a27f43a
Mateusz <mateuszkdev@users.noreply.github.com>
2021-01-15 17:10:48 +0100

    hmm

--- src/cache/main.ts
+++ src/cache/main.ts
@@ -3,13 +3,15 @@ import WebSocketManager from '../ws/WebSocketManager'
 export default class CacheManager {
 
     ws: WebSocketManager
+    cache: Map<number, any> // any but soon interface for messages, guild etc.
 
     constructor(ws: WebSocketManager){
 
         this.ws = ws
+        this.cache = new Map()
 
         this.ws.socket?.on('GUILD_CREATE', async (data: any) => {
-            // guild model required bruh
+            if (!this.cache.has(data.id)) this.cache.set(data.id, data)
         })
 
     }
