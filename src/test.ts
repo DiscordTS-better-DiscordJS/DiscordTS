d1776b1d92864351fa7850349ed06037f9f1a6a8
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-15 22:02:19 +0100

    fixed import.

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 const token: string = require('../token.json').token
 
-import Message from 'models/Message'
+import Message from './models/Message'
 import { Client } from './index'
 
 class bot extends Client {
