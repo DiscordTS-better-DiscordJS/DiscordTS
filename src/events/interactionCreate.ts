1bf145702f38e76c9b6fc1aaa94b04faf9044992
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-16 22:04:46 +0100

    interaction event. (slash commands)

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,17 @@
+import User from '../models/User'
+import Channel from '../models/Channel'
+import Client from '../models/Client'
+import { fetchChannel } from '../utils/fetch'
+
+export const _ = async (data: any, client: Client): Promise<any> => {
+    const object = { name: '', guild: {}, user: {}, roles: [], interaction: {}, channel: {} }
+
+    object.name = data.data.name
+    object.channel = new Channel(data.channel_id, client, await fetchChannel(data.channel_id))
+    object.user = new User(data.member.user)
+    object.roles = data.member.roles
+    object.guild = data.guild
+    object.interaction = data.data
+
+    return object
+}
