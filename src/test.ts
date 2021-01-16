bef8b065061b03b046a253dfcc84e57f46b08b4e
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-16 16:11:53 +0100

    resolve conflicts

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 const token: string = require('../token.json').token
 
 import Message from './models/Message'
-import { Client, SlashCommand } from './index'
+import { Client } from './index'
 
 class bot extends Client {
 
@@ -11,39 +11,21 @@ class bot extends Client {
 
         this.connect(token)
 
+
         this.on('message', async (message: Message) => {
 
-            //if (message.author.bot) return
-            
-            //console.log(this.cache, this.options)
-            
-            //message.channel.send(':ok:')
-            
+            if (message.author?.bot) return
+
+            console.log(this.cache.channels, this.cache.messages)
+
+            console.log(`event message`)
+
+            message.channel?.test
+
         })
-        
-        // To use SlashCommands you must login before.
-        new SlashCommand({ 
-            name: 'Hello', 
-            description: 'hello!', 
-            type: 1, // SUB_COMMAND
-            options: [
-                { 
-                    name: '1',
-                    description:'2', 
-                    type: 3, // STRING
-                    required: false,
-                    options: [
-                        {
-                            name: '3',
-                            description: '4',
-                            type: 4, // INTEGER
-                            required: true
-                        }
-                    ]
-                }
-            ]
-        }, this)
+
     }
+
 }
 
 new bot()
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -5,7 +5,8 @@ import Client from '../models/Client'
 const { token, bot } = require('../models/Client')
 
 export const sendMessage = async (content: any, channel_id: string) => {
-    let TOKEN = !bot ? token : `Bot ${token}`
+
+    let TOKEN = bot ? `Bot ${token}` : token
 
     content.tts = false
 
@@ -20,38 +21,14 @@ export const sendMessage = async (content: any, channel_id: string) => {
     return await res.json()
 }
 
-export const CreateSlashCommand = async (data: any, client: Client) => {
-    let token = client.options?.bot ? `Bot ${client.token}` : client.token
-    
-    if (client.options?.appID == '') return
+export const fetchChannel = async (id: string) => {
 
-    const command = Object.assign({
-        name: 'DiscordTS',
-        type: 1,
-        description: 'No description avaible.',
-        options: []
-    }, data)
-
-    const res = await fetch(`${Constants.API}/v8/applications/${client.options?.appID}/commands`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token
-        },
-        body: JSON.stringify(command)
-    })
-    
-    return await res.json()
-}
-
-export const fetchChannel = async (id: string, client: Client) => {
-    let token = client.options?.bot ? `Bot ${client.token}` : client.token
-
+    let TOKEN = bot ? `Bot ${token}` : token
     const channel = await fetch(`${Constants.API}/channels/${id}`, {
 
         method: "GET",
         headers: {
-            'Authorization': token
+            'Authorization': TOKEN
         }
 
     }) 
