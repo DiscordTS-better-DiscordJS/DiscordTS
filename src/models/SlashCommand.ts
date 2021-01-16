8f0e6ae8287c2c426ad79be34eecc57c1a7df3b6
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-16 22:59:49 +0100

    Update Slash command class and test.

--- src/models/SlashCommand.ts
+++ src/models/SlashCommand.ts
@@ -1,12 +1,18 @@
 import { options } from './Client'
 import { SlashCommandType } from '../types/SlashCommand'
-import { CreateSlashCommand } from '../utils/fetch'
+import { CreateSlashCommand, DeleteSlashCommands } from '../utils/fetch'
 
 export default class SlashCommand {
-    
-    constructor(data: SlashCommandType) {
+
+    constructor() {
         if (!options.token) console.log('You must to login first. Client.connect(token: string)')
-        else CreateSlashCommand(data)
     }
 
+    create(data: SlashCommandType) {
+        CreateSlashCommand(data)
+    }
+
+    delete(id: string) {
+        DeleteSlashCommands(id)
+    }
 }
--- src/test.ts
+++ src/test.ts
@@ -21,55 +21,15 @@ class bot extends Client {
         })
 
 
-        this.on('ready', () => {
-            /**
-             * This code comes from official discord docs.
-             * https://discord.com/developers/docs/interactions/slash-commands
-             */
-            /*new SlashCommand({
-                "name": "permissions",
-                "description": "Get or edit permissions for a user or a role",
-                "options": [
-                    {
-                        "name": "user",
-                        "description": "Get or edit permissions for a user",
-                        "type": 2, // 2 is type SUB_COMMAND_GROUP
-                        "options": [
-                            {
-                                "name": "get",
-                                "description": "Get permissions for a user",
-                                "type": 1 // 1 is type SUB_COMMAND
-                            },
-                            {
-                                "name": "edit",
-                                "description": "Edit permissions for a user",
-                                "type": 1
-                            }
-                        ]
-                    },
-                    {
-                        "name": "role",
-                        "description": "Get or edit permissions for a role",
-                        "type": 2,
-                        "options": [
-                            {
-                                "name": "get",
-                                "description": "Get permissions for a role",
-                                "type": 1
-                            },
-                            {
-                                "name": "edit",
-                                "description": "Edit permissions for a role",
-                                "type": 1
-                            }
-                        ]
-                    }
-                ]
-            })*/
-            new SlashCommand({
+        this.on('ready', async() => {
+            const Slash = new SlashCommand()
+
+            // Create slash command
+            Slash.create({
                 name: "test",
                 description: "Test command"
             })
+
             console.log('Bot is ready!')
         })
 
