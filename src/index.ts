f675b0fa57f91d079af2bdf4eca4cc2f55529839
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-17 19:45:44 +0100

    Added delete and all.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import WebSocketManager from './ws/WebSocketManager'
 import Client from './models/Client'
-import SlashCommand from './models/SlashCommand'
+import SlashCommands from './models/SlashCommand'
 import Collection from './models/Collection'
 
-export { WebSocketManager, Client, SlashCommand, Collection }
+export { WebSocketManager, Client, SlashCommands, Collection }
