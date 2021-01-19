0e9a2dd23dead1c2a0f0628509901680744dd040
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-19 22:03:10 +0100

    JsDoc in collection.

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,12 +1,17 @@
 
 
 export default class Collection<K, V> extends Map<K ,V> {
-
-    // maybe in future some options?
     constructor (options?: any) {
         super()
     }
     
+    /**
+     * Find key in Collection.
+     * @param {Function} Function to test with.
+     * @param {*} Value to use.
+     * @returns {*}
+     * @example new Collection().find(a => a.name === 'hello')
+     */
     find(method: (key: K, value: V, collection: this) => boolean): V | any {
         for (let [k, v] of this){
             if (method(k, v, this)) return v
