712323f559576e24ab436c105148573c02d48ff4
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-20 16:43:39 +0100

    permissions manager more methods (todo)

--- src/models/Permissions.ts
+++ src/models/Permissions.ts
@@ -12,10 +12,28 @@ export default class Permissions {
     }
 
     /**
-     * @TODO Check if permissions equals this.permissions
-     * @param {PermissionsType} permissions 
+     * @TODO Check if permissions equals this.permissions.
+     * @param {PermissionsType} permissions array.
      */
     check(permissions: PermissionsType) {
 
+    }
+
+    /**
+     * @TODO Convert permissions array to int.
+     * @param {PermissionsType} permissions array or this.
+     * @returns {string} permissions int.
+     */
+    toByte(permissions?: PermissionsType): string {
+        return ''
+    }
+
+    /**
+     * @TODO Convert permissions int to array.
+     * @param {string} permissions int.
+     * @returns {PermissionsType} permissions array.
+     */
+    toArray(permissions: string): PermissionsType {
+        return []
     }
 }
